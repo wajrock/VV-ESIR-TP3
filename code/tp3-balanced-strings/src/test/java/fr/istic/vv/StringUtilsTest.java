2f613d1e55cb07773bcb2554a49e50af6f673a31
Willthethreat8 <117753982+Willthethreat8@users.noreply.github.com>
2024-10-18 11:24:52 +0200

    modifs balanced strings

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -7,5 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    @Test
+    void testEmptyString() {
+        assertTrue(isBalanced(""), " The empty string is balanced.");
+    }
+
+    @Test
+    void testSingleBalancedPairs() {
+        assertTrue(isBalanced("()"), "balanced pair .");
+        assertTrue(isBalanced("[]"), "balanced pair.");
+        assertTrue(isBalanced("{}"), "balanced pair.");}
+
+    @Test 
+    void testSingleUnbalancedPairs(){
+        assertFalse(isBalanced(")("),"unbalanced pair");
+        assertFalse(isBalanced("}{"),"unbalanced pair");
+        assertFalse(isBalanced("]["),"unbalanced pair");
+    }
+    @Test 
+    void testmultipleBalancedPairs(){
+        assertTrue(isBalanced("(){}[]"), " balanced.");
+        assertTrue(isBalanced("{[()]}"), " balanced.");
+        assertTrue(isBalanced("{([])}"), "balanced.");
+        assertTrue(isBalanced("({[]})"), "balanced.");
+        assertTrue(isBalanced("([{}])"), "balanced.");
+        assertTrue(isBalanced("[({})]"), "balanced.");
+        assertTrue(isBalanced("[{()}]"), "balanced.");
+
+    }
+    
+
+
+
 
 }
