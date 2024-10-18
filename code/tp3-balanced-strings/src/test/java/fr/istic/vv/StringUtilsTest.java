8a1c603bcf8bede979e6584a746888d69e2d9f47
Willthethreat8 <117753982+Willthethreat8@users.noreply.github.com>
2024-10-18 14:19:19 +0200

    changes balanced strings

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -10,31 +10,86 @@ class StringUtilsTest {
     @Test
     void testEmptyString() {
         assertTrue(isBalanced(""), " The empty string is balanced.");
+    
+    
     }
 
     @Test
+    void testSingleOpeningWithoutClosing() {
+        assertFalse(isBalanced("["), "Opening square without a closing square.");
+        assertFalse(isBalanced("("), "Opening parenthesis without a closing parenthesis.");
+        assertFalse(isBalanced("{"), "Opening brace without a closing brace .");
+
+    }
+
+    @Test
+    void testSingleClosingWithoutOpening() {
+        assertFalse(isBalanced("}"), "Closing brace without an opening brace.");
+        assertFalse(isBalanced(")"), "Closing parenthesis without an opening parenthesis.");
+        assertFalse(isBalanced("]"), "Closing square bracket without an opening square bracket.");
+    }
+
+
+
+    @Test
     void testSingleBalancedPairs() {
-        assertTrue(isBalanced("()"), "balanced pair .");
+        assertTrue(isBalanced("()"), "balanced pair.");
         assertTrue(isBalanced("[]"), "balanced pair.");
         assertTrue(isBalanced("{}"), "balanced pair.");}
 
     @Test 
     void testSingleUnbalancedPairs(){
-        assertFalse(isBalanced(")("),"unbalanced pair");
-        assertFalse(isBalanced("}{"),"unbalanced pair");
-        assertFalse(isBalanced("]["),"unbalanced pair");
+        assertFalse(isBalanced(")("),"unbalanced pair.");
+        assertFalse(isBalanced("}{"),"unbalanced pair.");
+        assertFalse(isBalanced("]["),"unbalanced pair.");
+
+        
     }
     @Test 
     void testmultipleBalancedPairs(){
-        assertTrue(isBalanced("(){}[]"), " balanced.");
-        assertTrue(isBalanced("{[()]}"), " balanced.");
-        assertTrue(isBalanced("{([])}"), "balanced.");
-        assertTrue(isBalanced("({[]})"), "balanced.");
-        assertTrue(isBalanced("([{}])"), "balanced.");
-        assertTrue(isBalanced("[({})]"), "balanced.");
-        assertTrue(isBalanced("[{()}]"), "balanced.");
+        assertTrue(isBalanced("(){}[]"), "balanced pairs.");
+        assertTrue(isBalanced("{[()]}"), "balanced pairs.");
+        assertTrue(isBalanced("{([])}"), "balanced pairs.");
+        assertTrue(isBalanced("({[]})"), "balanced pairs.");
+        assertTrue(isBalanced("([{}])"), "balanced pairs.");
+        assertTrue(isBalanced("[({})]"), "balanced pairs.");
+        assertTrue(isBalanced("[{()}]"), "balanced pairs.");
+
+
 
     }
+
+    @Test 
+    void testmultipleUnBalancedPairs(){
+        assertFalse(isBalanced("{[()]}}"), "Unbalanced pairs.");
+        assertFalse(isBalanced("{[(])}"), "Unbalanced pairs.");
+        assertFalse(isBalanced("[({)]}"), "Unbalanced pairs.");
+        assertFalse(isBalanced("}{"), "Unbalanced pairs.");
+        assertFalse(isBalanced("()()}"), "Unbalanced pairs.");
+        assertFalse(isBalanced("((((("), "Unbalanced pairs.");
+        assertFalse(isBalanced("))))"), "Unbalanced pairs.");
+        assertFalse(isBalanced("([)]"), "Unbalanced pairs.");
+        assertFalse(isBalanced("{[}"), "Unbalanced pairs.");
+
+        //other tests
+
+        assertFalse(isBalanced("[(])"), "unbalanced pairs.");
+        assertFalse(isBalanced("((())"), "Unbalanced due to unmatched opening.");
+        assertFalse(isBalanced("())("), "Unbalanced due to unmatched closing.");
+
+    }
+
+
+    @Test
+    Void testBalancedNestedBrackets() {
+        
+        assertTrue(isBalanced("((((()))))"), "balanced pairs.");
+        assertTrue(isBalanced("{[({[()]})]}"), "balanced pairs .");
+    }
+}
+
+
+    
     
 
 
